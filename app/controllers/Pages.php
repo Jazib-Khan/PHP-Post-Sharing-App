e0dc9c0f6dfe0c99f5aa4bf85a74784f7cae5ab2
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-09-30 17:18:25 +0100

    Setup view and model methods in controller

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,11 +1,10 @@
 <?php
-    class Pages {
+    class Pages extends Controller {
         public function __construct() {
             
         }
 
         public function index() {
-            
         }
 
         public function about($id) {
