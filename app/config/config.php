ded235dd21b30d1985e8202b857cd226121074de
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-09-30 18:15:37 +0100

    Config File & Uploader

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+    // App Root
+    define ('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define ('URLROOT', 'http://localhost/php-post-sharing-app');
+    // Site Name
+    define ('SITENAME', 'PHP Post Sharing App');
