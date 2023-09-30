c9ba1eaf174ec48922105487ed85a8b3d1a8347f
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-09-30 17:47:17 +0100

    Loading views

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,10 +5,16 @@
         }
 
         public function index() {
+            $data = [
+                'title' => 'Welcome'
+            ];
+            
+            $this->view('pages/index', $data);
         }
 
-        public function about($id) {
-            echo $id;
+        public function about() {
+            $this->view('pages/about' );
+
         }
 
     }
