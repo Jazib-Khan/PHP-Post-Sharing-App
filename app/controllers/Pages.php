74df041d33e78c76212929f531f9f4b94780964d
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-10-01 13:43:14 +0100

    Database setup

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -6,7 +6,7 @@
 
         public function index() {
             $data = [
-                'title' => 'Welcome',
+                'title' => 'SharePosts',
             ];
             
             $this->view('pages/index', $data);
