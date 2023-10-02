2d4666336bcc2ffaefe1f8b0b132ab0ecf9b8107
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-10-02 16:06:46 +0100

    Post shows details page

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -8,6 +8,7 @@
             }
 
             $this->postModel = $this->model('Post');
+            $this->userModel = $this->model('User');
 
         }
 
@@ -71,6 +72,19 @@
 
             $this->view('posts/add', $data);
 
+        }
+
+        public function show($id) {
+
+            $post = $this->postModel->getPostById($id);
+            $user = $this->userModel->getUserById($post->user_id);
+
+            $data = [
+                'post' => $post,
+                'user' => $user
+            ];
+
+            $this->view('posts/show', $data);
         }
 
     }
--- app/models/Post.php
+++ app/models/Post.php
@@ -38,4 +38,15 @@
             }
         }
 
+        public function getPostById($id){
+
+            $this->db->query('SELECT * FROM posts WHERE id = :id');
+            $this->db->bind(':id', $id);
+
+
+            $row = $this->db->single();
+
+            return $row;
+        }
+
     }
