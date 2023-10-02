df89d736d96cd35e468ec18c4d16a3b3ba3c01d2
Jazib-Khan <57762628+Jazib-Khan@users.noreply.github.com>
2023-10-02 14:22:36 +0100

    Posts Access Control

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -28,5 +28,12 @@
                 }
             }
         }
+    }
 
+    function isLoggedIn(){
+        if (isset($_SESSION['user_id'])) {
+            return true;
+        } else {
+            return false;
+        }
     }
